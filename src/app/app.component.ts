6021bcb1aa9a2a15e048ad670d35d3d364b4cc4f
Ernesto Colina <ernesto.colina@zabud.com.co>
2022-10-01 22:53:26 -0500

    feat: styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'test-angular';
+  env = environment;
+
+  get getEnvironment(): 'Production' | 'Develop' {
+    return this.env.production ? 'Production' : 'Develop'
+  }
 }
